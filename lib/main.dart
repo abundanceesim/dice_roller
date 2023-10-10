ee823d5c5aa4084e60cf376b8dc2766af997941c
Abundance Esim <abundanceesim@gmail.com>
2023-10-10 14:48:53 -0400

    Create custom widget for app body

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,36 @@ void main() {
   // helps to render the GUI on screen. requires one arg (the widget or widget tree)
   // Widget for a single widget, and widget tree for multiple nested widgets.
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 229, 81, 255),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(103, 58, 183, 1), 
-                  Colors.purple
-                ], 
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight
-            ),
-                  
-          ),
-          child: const Center(
-            child: Text( 'Hello World!', style: TextStyle(color: Colors.white, fontSize: 28),),
-          ),
-        ),
+        body: GradientContainer()
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget{
+
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color.fromRGBO(103, 58, 183, 1), Colors.purple],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        );
+  }
+
+
 }
